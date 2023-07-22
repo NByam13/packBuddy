373fb9079b190031bb2e6821eafffde782ef192a
nbyam13 <nbyam13@gmail.com>
2023-07-22 15:08:54 -0400

    Change favicon, add index page and base layout

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <h2>Never forget your toothbrush again</h2>
+    <div>
+        <p>Promotional Text</p>
+        <p>More promotional Text</p>
+        <p>Testimonial 🙋‍♂️</p>
+    </div>
+</x-layout>
