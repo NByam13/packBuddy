1004c16fa10b997a1a5ac4f1cf4c706789f1b283
nbyam13 <nbyam13@gmail.com>
2023-07-23 12:38:23 -0400

    Add buttons for going back to dashboard and making new pack

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,9 @@
     <title>Pack Buddy</title>
     <link rel="icon" href="{{asset('favicon.ico')}}">
 
+    <!-- JS -->
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
--- resources/views/packs/index.blade.php
+++ resources/views/packs/index.blade.php
@@ -2,6 +2,13 @@
     <header class="pb-4 font-bold text-2xl text-center">
         <h2 class="border w-1/5 mx-auto rounded-2xl bg-white">All Packs</h2>
     </header>
+    <nav class="flex justify-between mb-4">
+        <button class="border border-gray-200 rounded-2xl px-4 py-2 bg-white hover:bg-gray-200">⬅️ Back</button>
+        <div class="flex items-center border border-gray-200 rounded-2xl px-4 py-2 bg-white hover:bg-gray-200">
+            <img class="w-8 h-8" alt="Add Icon" src="{{asset('images/icons8-add-48.png')}}"/>
+            New Pack
+        </div>
+    </nav>
     <main class="border border-gray-200 rounded-2xl bg-white px-8">
         <div class="grid grid-cols-5 font-bold text-gray-500 text-lg border-b-2">
             <div>Name</div>
@@ -11,7 +18,7 @@
             <div>Last Updated</div>
         </div>
         @foreach($packs as $pack)
-            <div class="grid grid-cols-5 border-b-2">
+            <div class="grid grid-cols-5 py-2 {{ !$loop->last ? "border-b-2" : ""}}">
                 <div>{{$pack->name}}</div>
                 <div>{{$pack->trips->count()}}</div>
                 <div>{{$pack->items->count()}}</div>
