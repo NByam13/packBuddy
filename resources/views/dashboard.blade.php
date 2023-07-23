8ac8dfc1dc27700fd3f2b00a3fe411035e097378
nbyam13 <nbyam13@gmail.com>
2023-07-22 22:52:14 -0400

    UI tweaks for packs

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,13 +2,17 @@
     <div class="grid space-x-4 grid-cols-3 mx-auto">
         <section class="row-span-3 border border-gray-200 rounded-lg py-2 px-4">
             <h2 class="text-xl border-b-2 mb-2">Your Packs</h2>
-            <div>
-                <div>
-                    @foreach($packs as $pack)
-                        <div>{{$pack->name}}</div>
-                        {{--                        <div>{{$pack->items->count()}}</div>--}}
-                    @endforeach
+            <div class="px-8">
+                <div class="grid grid-cols-2 font-bold text-gray-500 border-b-2 py-1">
+                    <div>Name</div>
+                    <div>Item Count</div>
                 </div>
+                @foreach($packs as $pack)
+                    <div class="grid grid-cols-2 py-2 {{ !$loop->last ? "border-b-2" : ""}}">
+                        <div>{{$pack->name}}</div>
+                        <div>5</div>
+                    </div>
+                @endforeach
             </div>
         </section>
         <section class="col-span-2 border border-gray-200 rounded-lg py-2 px-4 mb-4">
