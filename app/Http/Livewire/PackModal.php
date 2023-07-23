ad3d9504892ca7cd01bbd1a9415d3a06dfa14f77
nbyam13 <nbyam13@gmail.com>
2023-07-23 12:41:57 -0400

    Add new Livewire component for Pack Creation modal

---
+++ app/Http/Livewire/PackModal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class PackModal extends Component
+{
+    public function render()
+    {
+        return view('livewire.pack-modal');
+    }
+}
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,6 +9,7 @@
 
     <!-- JS -->
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    @livewireStyles
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -54,5 +55,6 @@
         {{ $slot }}
     </div>
 </div>
+@livewireScripts
 </body>
 </html>
