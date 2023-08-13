b2715f41e03236a77a30a2047c5372847afd54ce
nbyam13 <nbyam13@gmail.com>
2023-08-13 10:34:07 -0400

    Add trip index controller and page

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\TripResource;
+use App\Models\Trip;
 use Illuminate\Http\Request;
 
 class TripController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        return view('trips.index', [
+            'trips' => TripResource::collection(Trip::where('user_id', $request->user()->id)->get())
+        ]);
+    }
 }
---
+++ resources/views/trips/index.blade.php
@@ -0,0 +1,36 @@
+<x-layout>
+    <header class="pb-4 font-bold text-2xl text-center">
+        <h2 class="border w-1/5 mx-auto rounded-2xl bg-white">All Trips</h2>
+    </header>
+    <nav class="flex justify-between mb-4">
+        <livewire:back-button/>
+        <livewire:pack-modal/>
+    </nav>
+    <main class="border border-gray-200 rounded-2xl bg-white px-8">
+        <div class="grid grid-cols-5 font-bold text-gray-500 text-lg border-b-2">
+            <div>Name</div>
+            <div>Location</div>
+            <div>Start</div>
+            <div>End</div>
+            <div>Pack Count</div>
+        </div>
+        @if(count($trips))
+            @foreach($trips as $trip)
+                <div class="grid grid-cols-5 py-2 {{ !$loop->last ? "border-b-2" : ""}}">
+                    <div>{{$trip->name}}</div>
+                    <div>{{$trip->location}}</div>
+                    <div>{{$trip->start->toFormattedDateString()}}</div>
+                    <div>{{$trip->end->toFormattedDateString()}}</div>
+                    <div>{{$trip->packs->count()}}</div>
+                </div>
+            @endforeach
+        @else
+            <div class=" w-1/4 flex flex-col items-center mx-auto text-lg py-4 space-y-2">
+                <div>No Trips found! Add a new one!</div>
+                <div class="w-40">
+                    <livewire:pack-modal/>
+                </div>
+            </div>
+        @endif
+    </main>
+</x-layout>
